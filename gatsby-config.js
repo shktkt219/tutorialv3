3972a218834a0bcebb369b62eb941ffe07f11af1
shktkt219 <shktkt219@gmail.com>
2021-10-22 20:07:16 -0400

    finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,9 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
-      }
+        path: `${__dirname}/blog/`,
+      },
     },
+    "gatsby-plugin-mdx",
   ],
 };
